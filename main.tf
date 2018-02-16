7c3cec096f1e33626b15094055d66f86bd369ede
Tom Henderson <tomhenderson@mac.com>
2018-02-17 11:17:04 +1300

    Use shared slash command module

--- main.tf
+++ main.tf
@@ -7,12 +7,19 @@ provider "aws" {
 }
 
 module "catbot" {
-  source      = "slash_command"
+  source      = "github.com/tom-henderson/terraform-modules//slash-command"
   name        = "catbot"
   region      = "${var.region}"
   source_code = "catbot.zip"
+  module_name = "catbot"
+
+  ssm_parameters = ["catbot_slash_command_token"]
+
+  environment_variables = {
+    token_parameter = "catbot_slash_command_token"
+  }
 }
 
 output "command_url" {
-  value = "${module.catbot.command_url}"
+  value = "${module.catbot.slash_command_invocation_url}"
 }
